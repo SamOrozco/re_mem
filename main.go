ef282b152ed45b71854f3da789de7105ec28ccc5
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-03-26 23:57:20 -0700

    updating comment

--- main.go
+++ main.go
@@ -25,13 +25,19 @@ func queryStatement() {
 	if err != nil {
 		panic(err)
 	}
+	// get a "Query Statement" from the collection
+	// the query statement allows you to build complex queries
 	stmt := usersCollection.NewStatement()
 
+	// simple single value query
 	nameAbe := stmt.NewQuery("name", "abe")
+	// another simple query
 	nameSteven := stmt.NewQuery("email", "abe@g.com")
+	// combine those two with an and operator
 	both := stmt.NewQueryClause(nameAbe, nameSteven, re_mem.And)
+	// combine that combo with another query and the OR operator
 	combo := stmt.NewQueryClause(both, stmt.NewQuery("name", "steven"), re_mem.Or)
-
+	// fetch docs for all those queries
 	docs := combo.Fetch()
 	for _, val := range docs {
 		println(val.String())
