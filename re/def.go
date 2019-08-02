ca94098624a1778f7d38e2f9ce57941004eb02a5
Sam Orozco <samuel.j.orozco@gmail.com>
2019-08-01 23:00:16 -0600

    commenting the collection

--- re/def.go
+++ re/def.go
@@ -10,13 +10,21 @@ type Storage interface {
 }
 
 type Collection interface {
+	// get a json object for the given row key
 	Get(key string) (data.JsonMap, error)
+	// creates an entry in the collection from the interface passed to this method
 	Create(doc interface{}) (string, error)
+	// removes the object with the given row key
 	Remove(key string) error
+	// executes a simple query based on the column and the given value
 	Query(column, value string) ([]data.JsonMap, error)
+	// executes a query based on the given SingleQuery
 	ExecuteQuery(query *SingleQuery) ([]data.JsonMap, error)
+	// returns a new query statement for the given location
 	NewStatement() Statement
+	// gets all row keys where the records has the given value for the given column
 	GetRowKeys(colName, stringValue string) []string
+	// gets all rows for the given keys
 	GetRowsForKeys(keys []string) []data.JsonMap
 }
 
